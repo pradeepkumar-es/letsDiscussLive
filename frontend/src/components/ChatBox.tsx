fc48ff07e0df91d14ac58660201a7764dd28fe62
pradeepkumar-es <createthefuture121@gmail.com>
2025-08-21 00:31:52 +0530

    update

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -4,7 +4,7 @@ import type { Me } from "../App";
 import { useNavigate } from "react-router-dom";
 import { useRelativeTime } from "../lib/useRelativeTime";
 
-const socket = io("http://localhost:5000", { withCredentials: true });
+const socket = io("https://letsdiscusslive-1.onrender.com", { withCredentials: true });
 
 export default function ChatBox({
   postId,
