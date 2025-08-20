7eb0c3534b9f03bab69533ca8fc5293185cc581c
pradeepkumar-es <createthefuture121@gmail.com>
2025-08-20 21:37:39 +0530

    backend live url update for api

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://letsdiscusslive.onrender.com/api",
   withCredentials: true // send/receive auth cookie
 });
