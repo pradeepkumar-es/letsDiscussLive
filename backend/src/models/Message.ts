903663158e071b6f2b9b3d4697ef701c0493cd02
pradeepkumar-es <createthefuture121@gmail.com>
2025-08-20 21:29:47 +0530

    message model update

--- backend/src/models/Message.ts
+++ backend/src/models/Message.ts
@@ -2,15 +2,28 @@ import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
   {
-    postId:   { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true, index: true },
-    userId:   { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+      index: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     username: { type: String, required: true },
-    text:     { type: String, required: true, maxlength: 2000 },
+    text: { type: String, required: true, maxlength: 2000 },
 
     // for replies
-    parentId:       { type: mongoose.Schema.Types.ObjectId, ref: "Message", default: null },
+    parentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+      default: null,
+    },
     parentUsername: { type: String, default: null }, // for display of whom this message replied to
-    parentText:     { type: String, default: null }  // for displaying message that is being replied
+    parentText: { type: String, default: null }, // for displaying message that is being replied
   },
   { timestamps: true }
 );
@@ -23,6 +36,8 @@ export interface IMessage extends mongoose.Document {
   parentId?: mongoose.Types.ObjectId | null;
   parentUsername?: string | null;
   parentText?: string | null;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export default mongoose.model<IMessage>("Message", messageSchema);
