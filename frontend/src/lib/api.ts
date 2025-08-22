9a533f47c2d0f91f1d5683f61dfd814bae601cf1
pradeepkumar-es <createthefuture121@gmail.com>
2025-08-22 11:12:02 +0530

    update

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: import.meta.env.BACKEND_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.BACKEND_API_URL,
   withCredentials: true // send/receive auth cookie
 });
