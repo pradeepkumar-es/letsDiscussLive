d5e21a0371b44fa6b3bc6ba03da2ff29acf35067
Pradeep Kumar <102599114+pradeepkumar-es@users.noreply.github.com>
2025-08-22 16:45:34 +0530

    Update auth.controller.ts

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -9,7 +9,7 @@ const cookieOpts: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",       // true in production, false in dev
   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // TS-safe union type
-  domain: process.env.NODE_ENV === "production" ? ".onrender.com" : undefined, // domain only for prod
+  // domain: process.env.NODE_ENV === "production" ? ".onrender.com" : undefined, // domain only for prod
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
